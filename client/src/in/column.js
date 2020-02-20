d22e3e638423aa21ba00d8515da0eeabeb988422
noidname01 <tim983649@gmail.com>
2020-02-20 14:20:18 +0800

    20200220_2

--- client/src/in/column.js
+++ client/src/in/column.js
@@ -31,7 +31,9 @@ class Column extends Component{
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;創立於2017年的BravoAI (洽吧智能)是一間AI公司，專注於研發、提供金融機構廣泛的軟體服務。運用其電腦視覺、自然語言處理、深度學習等AI專長，BravoAI協助人壽與保險公司研發核保、理賠等流程自動化的方案，不僅替客戶節省大量行政成本，也為公司打出了知名度。目前為止，BravoAI以達成台灣壽險市佔率第一，更計畫邁向國際化。<br/>
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BravoAI主要由創辦人趙式隆、執行長林奕辰與技術長沈昇勳帶領，團隊著重於技術基礎研究與應用技術解決實際問題。
 					</div>
+					
 				</div>
+				<a id="column_readmore" href="#">read more...</a>
 			</div>
 			<div id = "column_section">
 				<div id = "column_img"></div>
@@ -54,6 +56,7 @@ class Column extends Component{
 					</div>
 					
 				</div>
+				<a id="column_readmore" href="#">read more...</a>
 			</div>
 			<div id = "column_section">
 				<div id = "column_img"></div>
@@ -74,7 +77,9 @@ class Column extends Component{
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2008畢業於台大電機，目前任職於臺灣科技大學的方劭云教授，僅僅不到十年的時間內已經取得了副教授的頭銜，能有這樣的榮譽，是背後無數國際期刊論文與比賽獲獎的支撐。<br/>
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;方教授於IEEE/ACM網絡學報屢次發布論文，二度獲得該學報最佳論文獎的榮譽。自2016起更帶領台科大團隊於CAD Contest at ICCAD比賽中四度獲獎，為臺灣在EDA領域中頗具地位的人才。目前授課項目包括VLSI、邏輯設計、演算法、奈米積體電路實體設計，並持續於EDA、奈米積體電路實體設計、製造可行性/可靠性設計、ML設計最佳化等領域中深造。<br/>
 					</div>
+					
 				</div>
+				<a id="column_readmore" href="#">read more...</a>
 			</div>
 			<div id = "column_section">
 				<div id = "column_img"></div>
@@ -95,8 +100,11 @@ class Column extends Component{
 					<div id = "column_prefix">
 						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;王易如於史丹佛大學取得碩士學位後，經由世界最大黑克松Salesforce $1Million Hackathon籌組MoBagel（行動貝果），目前任職該新創的營運長。MoBagel 致力於讓機器學習落地，目標讓人工智慧普及化，主要方向為APU (Advanced Preprocessing Unit)，根據AI的應用類別來進行資料前處理，增加預測準確度，目前已經與台灣許多產業龍頭合作。
 					</div>
+					
 				</div>
+				<a id="column_readmore" href="#">read more...</a>
 			</div>
+			
 		</div>
         )
     }
