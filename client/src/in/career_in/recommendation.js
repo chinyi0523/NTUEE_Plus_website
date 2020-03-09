f17666320926e0179cb58901d05d8182607d91c8
chinyi0523 <james31423a@gmail.com>
2020-03-10 02:39:48 +0800

    small modify of career pages

--- client/src/in/career_in/recommendation.js
+++ client/src/in/career_in/recommendation.js
@@ -5,80 +5,89 @@ import {NavBar_in} from '../../component/AppBar_in';
 class Recommendation extends Component{
     render(){
         return (
-			<div id = "FindJob_container">
-				<div id = "FindJob_section" style={{marginTop:"8%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+			<div id = "recommendation_container">
+				<div id = "recommendation_section" style={{marginTop:"8%"}} >
+					<div id = "recommendation_section_1" >
+						<div id = "recommendation_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recommendation_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recommendation_section_2" >
+						<div id="recommendation_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
-								職缺：test
+						<div id = "recommendation_text">
+							<div id = "recommendation_info">
+								專業：test
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recommendation_info">
+								期望公司：test
+							</div>
+							<div id = "recommendation_info">
 								簡介：test
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recommendation_info">
 								聯絡：test
 							</div>
 						</div>
 					</div>		
 				</div>
-				<div id = "FindJob_section" style={{marginTop:"3%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+				<div id = "recommendation_section" style={{marginTop:"3%"}} >
+					<div id = "recommendation_section_1" >
+						<div id = "recommendation_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recommendation_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recommendation_section_2" >
+						<div id="recommendation_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
-								職缺：
+						<div id = "recommendation_text">
+							<div id = "recommendation_info">
+								專業：
 							</div>
-							<div id = "FindJob_info">
-								簡介：
+							<div id = "recommendation_info">
+								期望公司：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recommendation_info">
+								簡介：test
+							</div>
+							<div id = "recommendation_info">
 								聯絡：
 							</div>
 						</div>
 					</div>		
 				</div>
-				<div id = "FindJob_section" style={{marginTop:"3%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+				<div id = "recommendation_section" style={{marginTop:"3%"}} >
+					<div id = "recommendation_section_1" >
+						<div id = "recommendation_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recommendation_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recommendation_section_2" >
+						<div id="recommendation_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
-								職缺：
+						<div id = "recommendation_text">
+							<div id = "recommendation_info">
+								專業：
 							</div>
-							<div id = "FindJob_info">
-								簡介：
+							<div id = "recommendation_info">
+								期望公司：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recommendation_info">
+								簡介：test
+							</div>
+							<div id = "recommendation_info">
 								聯絡：
 							</div>
 						</div>
--- client/src/in/career_in/recruitment.js
+++ client/src/in/career_in/recruitment.js
@@ -5,80 +5,80 @@ import {NavBar_in} from '../../component/AppBar_in';
 class Recruitment extends Component{
     render(){
         return (
-			<div id = "FindJob_container">
-				<div id = "FindJob_section" style={{marginTop:"8%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+			<div id = "recruitment_container">
+				<div id = "recruitment_section" style={{marginTop:"8%"}} >
+					<div id = "recruitment_section_1" >
+						<div id = "recruitment_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recruitment_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recruitment_section_2" >
+						<div id="recruitment_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
+						<div id = "recruitment_text">
+							<div id = "recruitment_info">
 								職缺：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								簡介：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								聯絡：
 							</div>
 						</div>
 					</div>		
 				</div>
-				<div id = "FindJob_section" style={{marginTop:"3%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+				<div id = "recruitment_section" style={{marginTop:"3%"}} >
+					<div id = "recruitment_section_1" >
+						<div id = "recruitment_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recruitment_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recruitment_section_2" >
+						<div id="recruitment_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
+						<div id = "recruitment_text">
+							<div id = "recruitment_info">
 								職缺：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								簡介：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								聯絡：
 							</div>
 						</div>
 					</div>		
 				</div>
-				<div id = "FindJob_section" style={{marginTop:"3%"}} >
-					<div id = "FindJob_section_1" >
-						<div id = "FindJob_title">
+				<div id = "recruitment_section" style={{marginTop:"3%"}} >
+					<div id = "recruitment_section_1" >
+						<div id = "recruitment_title">
 							title
 						</div>
-						<div id = "FindJob_subtitle">
+						<div id = "recruitment_subtitle">
 							subtitle
 						</div>
 					</div>
-					<div id = "FindJob_section_2" >
-						<div id="FindJob_img">
+					<div id = "recruitment_section_2" >
+						<div id="recruitment_img">
 							<img/>
 						</div>
-						<div id = "FindJob_text">
-							<div id = "FindJob_info">
+						<div id = "recruitment_text">
+							<div id = "recruitment_info">
 								職缺：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								簡介：
 							</div>
-							<div id = "FindJob_info">
+							<div id = "recruitment_info">
 								聯絡：
 							</div>
 						</div>
