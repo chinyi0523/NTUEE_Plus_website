f2a3d29b5968c0c03e758264229ccf8116172f4a
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-03-26 20:32:58 +0800

    file limit 100MB

--- routes/middleware/multer.js
+++ routes/middleware/multer.js
@@ -5,8 +5,8 @@ var multer = require('multer')
 
 const upload=multer({
 	limits: {
-		// 限制上傳檔案的大小為 1MB
-		fileSize: 1000000
+		// 限制上傳檔案的大小為 100MB
+		fileSize: 100000000
 	},
 	fileFilter:function(req,file,cb){
 		if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
