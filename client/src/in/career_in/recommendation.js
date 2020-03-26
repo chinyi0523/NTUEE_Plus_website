64f53926879f9dd7bdc1aabb543503830395db55
martinooh <55379230+martinooh@users.noreply.github.com>
2020-03-26 19:05:42 +0800

    Update recommendation.js

--- client/src/in/career_in/recommendation.js
+++ client/src/in/career_in/recommendation.js
@@ -5,95 +5,95 @@ import {NavBar_in} from '../../component/AppBar_in';
 class Recommendation extends Component{
     render(){
         return (
-			<div id = "recommendation_container">
-				<div id = "recommendation_section" style={{marginTop:"8%"}} >
-					<div id = "recommendation_section_1" >
-						<div id = "recommendation_title">
-							title
+		<div id = "recommendation_container">
+			<div id = "recommendation_section">
+				<div id = "recommendation_section_1" >
+					<div id = "recommendation_title">
+						title
+					</div>
+					<div id = "recommendation_subtitle">
+						subtitle
+					</div>
+				</div>
+				<div id = "recommendation_section_2" >
+					<div id="recommendation_img">
+						<img/>
+					</div>
+					<div id = "recommendation_text">
+						<div id = "recommendation_info">
+							專業：test
 						</div>
-						<div id = "recommendation_subtitle">
-							subtitle
+						<div id = "recommendation_info">
+							期望公司：test
+						</div>
+						<div id = "recommendation_info">
+							簡介：test
+						</div>
+						<div id = "recommendation_info">
+							聯絡：test
 						</div>
 					</div>
-					<div id = "recommendation_section_2" >
-						<div id="recommendation_img">
-							<img/>
-						</div>
-						<div id = "recommendation_text">
-							<div id = "recommendation_info">
-								專業：test
-							</div>
-							<div id = "recommendation_info">
-								期望公司：test
-							</div>
-							<div id = "recommendation_info">
-								簡介：test
-							</div>
-							<div id = "recommendation_info">
-								聯絡：test
-							</div>
-						</div>
-					</div>		
-				</div>
-				<div id = "recommendation_section" style={{marginTop:"3%"}} >
-					<div id = "recommendation_section_1" >
-						<div id = "recommendation_title">
-							title
-						</div>
-						<div id = "recommendation_subtitle">
-							subtitle
-						</div>
-					</div>
-					<div id = "recommendation_section_2" >
-						<div id="recommendation_img">
-							<img/>
-						</div>
-						<div id = "recommendation_text">
-							<div id = "recommendation_info">
-								專業：
-							</div>
-							<div id = "recommendation_info">
-								期望公司：
-							</div>
-							<div id = "recommendation_info">
-								簡介：test
-							</div>
-							<div id = "recommendation_info">
-								聯絡：
-							</div>
-						</div>
-					</div>		
-				</div>
-				<div id = "recommendation_section" style={{marginTop:"3%"}} >
-					<div id = "recommendation_section_1" >
-						<div id = "recommendation_title">
-							title
-						</div>
-						<div id = "recommendation_subtitle">
-							subtitle
-						</div>
-					</div>
-					<div id = "recommendation_section_2" >
-						<div id="recommendation_img">
-							<img/>
-						</div>
-						<div id = "recommendation_text">
-							<div id = "recommendation_info">
-								專業：
-							</div>
-							<div id = "recommendation_info">
-								期望公司：
-							</div>
-							<div id = "recommendation_info">
-								簡介：test
-							</div>
-							<div id = "recommendation_info">
-								聯絡：
-							</div>
-						</div>
-					</div>		
-				</div>
+				</div>		
 			</div>
+			<div id = "recommendation_section" style={{marginTop:"3%"}} >
+				<div id = "recommendation_section_1" >
+					<div id = "recommendation_title">
+						title
+					</div>
+					<div id = "recommendation_subtitle">
+						subtitle
+					</div>
+				</div>
+				<div id = "recommendation_section_2" >
+					<div id="recommendation_img">
+						<img/>
+					</div>
+					<div id = "recommendation_text">
+						<div id = "recommendation_info">
+							專業：
+						</div>
+						<div id = "recommendation_info">
+							期望公司：
+						</div>
+						<div id = "recommendation_info">
+							簡介：test
+						</div>
+						<div id = "recommendation_info">
+							聯絡：
+						</div>
+					</div>
+				</div>		
+			</div>
+			<div id = "recommendation_section" style={{marginTop:"3%"}} >
+				<div id = "recommendation_section_1" >
+					<div id = "recommendation_title">
+						title
+					</div>
+					<div id = "recommendation_subtitle">
+						subtitle
+					</div>
+				</div>
+				<div id = "recommendation_section_2" >
+					<div id="recommendation_img">
+						<img/>
+					</div>
+					<div id = "recommendation_text">
+						<div id = "recommendation_info">
+							專業：
+						</div>
+						<div id = "recommendation_info">
+							期望公司：
+						</div>
+						<div id = "recommendation_info">
+							簡介：test
+						</div>
+						<div id = "recommendation_info">
+							聯絡：
+						</div>
+					</div>
+				</div>		
+			</div>
+		</div>
         )
     }
 }
