7077679a3281adb8a8829712f4fca82b49105271
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-03-26 20:40:17 +0800

    file accept JPG

--- routes/middleware/multer.js
+++ routes/middleware/multer.js
@@ -9,7 +9,7 @@ const upload=multer({
 		fileSize: 100000000
 	},
 	fileFilter:function(req,file,cb){
-		if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+		if(!file.originalname.match(/\.(jpg|jpeg|png|JPG|PNG|JPEG)$/)){
 			req.fileValidationError = '檔案格式錯誤'
 			return cb(new Error('檔案格式錯誤'),false)
 		}
