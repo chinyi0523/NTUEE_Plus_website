30717e3fd12a0aa1e6b269bcfe2984a9e0711699
Darren221 <47858999+Darren221@users.noreply.github.com>
2020-04-09 21:11:47 +0800

    Update searchJob.js

--- routes/srcs/in/searchJob.js
+++ routes/srcs/in/searchJob.js
@@ -3,26 +3,25 @@ var readDB = require('./readDB');
 
 module.exports = function (req, res, next) {
 	var jobTitle = req.body.title
-	if(!jobTitle){
-		jobTitle = 'Job Title' //測試用
-	}
+
 	if(jobTitle){
-		var query = readDB.search(req);
-		job_Schema.find(query,{_id:0},function(err,obj){
+		var query = readDB.search_job(req);
+		job_Schema.find(query, {_id:0}, function(err,obj){
 			if (err) {
 				console.log("Error:" + err);
 				return res.send({status:'success',message:false, description:"資料庫錯誤"}); 
 			}
 			else{
 				var output = []
-				obj.forEach(people=>{
-					var output1 = readDB.getOtherDB(people)
+				obj.forEach(item=>{
+					var output1 = readDB.getOtherDB_job(item)
 					output.push(output1)
 				})
 				return res.send({status:'success',message:true,data:output});
 			}
 		})
-	}else{
-		res.send({status:'success',message:false,description:"session不存在(已過期)"});
+	}
+	else{
+		res.send({status:'success', message:false, description:"session不存在(已過期)"});
 	}
 }
