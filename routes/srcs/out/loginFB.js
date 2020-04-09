ba11c96571d7dc66373c123a711cb55bb24692a9
Jian-Han0504 <jh880507@gmail.com>
2020-04-09 17:57:46 +0800

    add backend Facebook Login & Register js files

---
+++ routes/srcs/out/loginFB.js
@@ -0,0 +1,35 @@
+//srcs/login.js
+var user_l_Schema = require('../../Schemas/user_login');
+
+module.exports = function (req, res, next) {
+	var facebookID = req.body.facebookID;
+	var query = {facebookID: facebookID};
+	user_l_Schema.find(query, function(err, obj){
+		if (err) {
+			console.log("Error:" + err);
+			return res.send({status:'success',message:false,description:"資料庫發生錯誤"});
+		}
+		else {
+			if(obj.length == 1){
+				console.log('登入成功',obj);
+				req.session.regenerate(function(err) {
+					if(err){
+						console.log("session建立失敗，err=\n",err);
+						return res.send({
+							status:'success',
+							message:false,
+							description:"session建立失敗"
+							});                
+					}
+					req.session.loginName = obj[0].username;
+					req.session.loginAccount = obj[0].account;
+					return res.send({status:'success',message:true,data:{username:obj[0].username,account:obj[0].account}});
+					//res.redirect('/');
+					});
+			}else{
+				console.log('帳號不存在');
+				return res.send({status:'success',message:false,description:"帳號不存在"}); 
+			}
+		}
+	})
+}
