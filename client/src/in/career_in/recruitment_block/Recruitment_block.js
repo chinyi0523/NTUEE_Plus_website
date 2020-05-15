631ed84b42b449e172cf2d18fff035c3ef25bc4f
anitalu724 <b06901011@ntu.edu.tw>
2020-05-15 23:19:34 +0800

    20200515_anitalu_recruitment

--- client/src/in/career_in/recruitment_block/Recruitment_block.js
+++ client/src/in/career_in/recruitment_block/Recruitment_block.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Recruitment_block.css';
+import Recruitment_block_requirement from './Recruitment_requirement'
+
 
 
 const Recruitment_block = (props) =>{
     let recruit_image = props.data.image
     let recruit_title = props.data.title
     let recruit_info = props.data.info
-    let recruit_description = props.data.description
+    let recruit_spec = props.data.spec
     let id = props.data.id
 
     let visual_image = null
@@ -15,51 +17,19 @@ const Recruitment_block = (props) =>{
     }
     return(
         <div id="Recruitment_block_container">
-            {visual_image}
-            <Recruitment_title title={recruit_title} id={id+'_title'}/>
-            <Recuritment_info info={recruit_info} id={id+'_info'}/>
-            <Recruitment_description description={recruit_description} id={id+'_description'}/>
-            
+            <section>
+                {visual_image}
+                <Recruitment_title title={recruit_title} id={id+'_title'}/>
+            </section>
+            <section>
+                <Recruitment_info info={recruit_info} id={id+'_info'}/>          
+                <Recruitment_spec spec={recruit_spec} id={id+'_spec'}/>
+            </section>
         </div>
     )
 }
 
 export default Recruitment_block;
-
-const Recruitment_title = (props) => {
-    let title = props.title;
-    let id = props.id;
-    return (
-        <div>
-            <h1 className='Recruitment_block_title' id={id}>{title}</h1>
-        </div>
-    )
-}
-
-const Recuritment_info = (props) =>{
-    let company_name = props.info.company_name;
-    let work_type = props.info.work_type;
-	let salary = props.info.salary;
-	let requirement = props.info.requirement
-    let diploma = props.info.diploma
-    let id = props.id
-    return(
-        <div>
-            <p className="Recruitment_block_info">{company_name} | {work_type}</p>
-            <p className="Recruitment_block_info">{salary} | {requirement} | {diploma}</p>
-        </div>
-    )
-}
-
-const Recruitment_description = (props) => {
-    let description = props.description;
-    let id = props.id
-    return (
-        <div>
-            <p className='Recruitment_block_description' id={id}>{description}</p>
-        </div>
-    )
-}
 
 const Recruitment_image = (props) => {
     let image = props.image;
@@ -70,3 +40,54 @@ const Recruitment_image = (props) => {
         </div>
     )
 }
+
+const Recruitment_title = (props) => {
+    let title = props.title.title;
+    let company_name = props.title.company_name;
+    let work_type = props.title.work_type;
+    let id = props.id;
+    return (
+        <div className='Recruitment_block_title_container'>
+            <p className='Recruitment_block_title' id={id}>{title}</p>
+            <p className="Recruitment_block_subtitle">{company_name} | {work_type}</p>
+        </div>
+    )
+}
+
+const Recruitment_info = (props) =>{
+	let salary = props.info.salary;
+	let experience = props.info.experience;
+    let diploma = props.info.diploma;
+    let id = props.id;
+    return(
+        <div className="Recruitment_block_info_div">
+            <p className="Recruitment_block_info">
+                <td className="Recruitment_block_salary">{salary} </td>
+                <td>  |  {experience} |  {diploma}</td>
+            </p>
+        </div>
+    )
+}
+
+const Recruitment_spec = (props) => {
+    let requirement = props.spec.requirement.split('\n').map(i => {
+        return <li>{i}</li>
+    });
+    let description = props.spec.description.split('\n').map(i => {
+        return <li>{i}</li>
+    });
+    let id = props.id;
+    return (
+        <div className="Recruitment_block_spec_div">
+            <p className="Recruitment_block_requirement" id={id}>
+                <li id ="Recruitment_requirement_li">要求條件：</li>
+                {requirement}
+            </p>
+            <p className='Recruitment_block_requirement' id={id}>
+                <li id ="Recruitment_requirement_li">說明：</li>
+                {description}
+            </p>
+        </div>
+    )
+}
+
