3fb389a939c9f711671c944a84274052643798ac
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-07-02 19:50:40 +0800

    db update

--- routes/Schemas/db.js
+++ routes/Schemas/db.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose')
 
 //數據地址
-DB_URL = "mongodb://heroku_b6klgxdz:lmed4cj2a50535mbei4fnsfq58@ds213529.mlab.com:13529/heroku_b6klgxdz"
+DB_URL = "mongodb://heroku_kbtrwz4h:f13g3thhm7uo2ip6o2qcnaufk9@ds155718.mlab.com:55718/heroku_kbtrwz4h"
+//"mongodb://heroku_b6klgxdz:lmed4cj2a50535mbei4fnsfq58@ds213529.mlab.com:13529/heroku_b6klgxdz"
 // ; // || process.env.MONGODB_URI || 'mongodb://localhost:27017/mongoose';
 
 mongoose.connect(DB_URL);
@@ -13,3 +14,4 @@ mongoose.connection.on('disconnected',function(){
 
 module.exports = mongoose;
 //mongodb://heroku_kbtrwz4h:f13g3thhm7uo2ip6o2qcnaufk9@ds155718.mlab.com:55718/heroku_kbtrwz4h
+//mongodb://heroku_b6klgxdz:lmed4cj2a50535mbei4fnsfq58@ds213529.mlab.com:13529/heroku_b6klgxdz
