c24ac3d53f3fb476b041ed0555cae04b62827f79
chinyi0523 <james31423a@gmail.com>
2020-07-19 18:00:50 +0800

    support-grid-finish

--- client/src/out/Support.js
+++ client/src/out/Support.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import './Support.css';
-import { NavBar } from '../component/AppBar';
 import Footer from "../component/Footer/Footer";
 class Support extends Component{
     render(){
         return (
         <div>
-        <div id="Support_container" class="col-8 offset-2 col-md-6 offset-md-3">
+        <div id="Support_container" class="col-8 offset-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div id="Support_up">
-                <h3 id="Support_up_text" >
+                <p id="Support_up_text" >
                     Your support is vital in enabling NTUEE+ to fulfill
                     our mission to chain all the alumnae in the world.
-                </h3>
+                </p>
             </div>
             <div id="Support_down">
                 <p id="Support_down_text">
