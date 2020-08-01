67c3234222dddb8fa4316364c02ca883db627590
GinGerBread-Yellow <andyhuang0323@gmail.com>
2020-08-01 16:15:08 +0800

    Change Column_block and Column_block webpage, Set up _global.css to integrate color style

--- client/src/in/column_block/column_block.js
+++ client/src/in/column_block/column_block.js
@@ -1,39 +1,103 @@
-import React, { Component } from "react";
-import Column_block_anno from "./column_block_anno";
-import Column_block_title from './column_block_title';
-import Column_block_exp from './column_block_exp';
-import Column_block_edu from './column_block_edu';
-import Column_block_intro from './column_block_intro';
-import Column_block_img from './column_block_img';
+import React, { Component } from 'react';
 import './column_block.css';
 
-class Column_block extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            filename: this.props.block.filename,
-			anno : this.props.block.anno,
-            title : this.props.block.title,
-            exp : this.props.block.exp,
-            edu : this.props.block.edu,
-            intro : this.props.block.intro,
-            id : this.props.block.id,
-        }
-    }
-    render(){
-        return(
-            <div id = "column_block">
-                <Column_block_img id={`${this.state.id}_img`} filename={this.state.filename}/>
-                <div id= "column_block_text">
-				<Column_block_anno id = {`${this.state.id}_anno`} anno = {this.state.anno}/>
-                <Column_block_title id = {`${this.state.id}_title`} title = {this.state.title}/>
-				<Column_block_exp id = {`${this.state.id}_exp`} exp = {this.state.exp}/>
-				<Column_block_edu id = {`${this.state.id}_edu`} edu = {this.state.edu}/>
-				<Column_block_intro id = {`${this.state.id}_intro`} intro = {this.state.intro}/>
+const Column_block_title = (props) =>{
+    const _id = props.id;
+    const _title = props.title;
+    const Listitems = _title.map((Intro) =>
+    <p>{Intro}</p>
+    );
+    return (
+        <span id= {_id}  class="column_block_title">{Listitems}</span>
+    );
+}
+const Column_block_exp = (props) =>{
+    const _id = props.id;
+    const _exp = props.exp;
+    const Listintro = _exp.map((Intro) =>
+    <p>{Intro}</p>
+    );
+    return (
+        <p id= {_id}  class="column_block_introtext">{Listintro}</p>
+    );
+}
+const Column_block_edu = (props) =>{
+    const _id = props.id;
+    const _edu = props.edu;
+    const Listintro = _edu.map((Intro) =>
+    <p>{Intro}</p>
+    );
+    return (
+        <p id= {_id}  class="column_block_introtext">{Listintro}</p>
+    );
+}
+const Column_block_intro = (props) =>{
+    const _id = props.id;
+    const _intro = props.intro;
+    const Listintro = _intro.map((Intro) =>
+    <p>{Intro}</p>
+    );
+    return (
+        <p id= {_id}  class="column_block_introtext">{Listintro}</p>
+    );
+}
+const Column_block_anno = (props) =>{
+    const _id = props.id;
+    const _anno = props.anno;
+    const _anno_name = _anno[0];
+    const _anno_date = _anno[1];
+    return (
+        <p id={_id} class="column_block_Anno">
+            <span className="column_block_anno">{_anno_name}</span>
+            <span className="column_block_date">{_anno_date}</span>
+        </p>
+    );
+}
+const Column_block_img = (props) =>{
+    const _id = props.id;
+    const _img = props.img;
+    return (
+        <div className="column_block_img">
+            <img src={_img} alt = {_id}/>
+        </div>
+    );
+}
+
+const Column_block = (props) =>{
+    const _link = props.link;
+    const _filename = props.block.filename;
+    const _anno = props.block.anno;
+    const _title = props.block.title;
+    const _exp = props.block.exp;
+    const _edu = props.block.edu;
+    const _intro = props.block.intro;
+    const _id = props.block.id;
+    return (
+        <a href={_link} id="column_block" class ="container">
+            <div class="row">
+                
+                <div class="col-12 col-lg-4" >  
+                    <Column_block_img id={`${_id}_img`} filename={_filename}/>
+                </div>
+                <div class="col-12 col-lg-8 column_block_text"> 
+                    
+                    <Column_block_title id = {`${_id}_title`} title = {_title}/> 
+                    <div class="Simple-Line"></div>    
+                    <div class="column_block_bulletlist">
+                        
+                        <li class="column_listtitle">經歷</li>
+                        <Column_block_exp id = {`${_id}_exp`} exp = {_exp}/>
+                        <li class="column_listtitle">學歷</li>
+                        <Column_block_edu id = {`${_id}_edu`} edu = {_edu}/>
+                        <li class="column_listtitle">介紹</li>
+                        <Column_block_intro id = {`${_id}_intro`} intro = {_intro}/> 
+                        <Column_block_anno id={`${_id}_img`} anno={_anno}/> 
+                    </div>
                 </div>
             </div>
-        )
-    }
+        </a>
+    );
 }
 
 export default Column_block
+
