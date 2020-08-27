baeac935304c741565e9dd3a08473ab9f4a982a7
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-08-27 15:47:07 +0800

    forget mail change

--- routes/srcs/out/mail/credential.js
+++ routes/srcs/out/mail/credential.js
@@ -2,7 +2,7 @@
 module.exports = {
   cookieSecret: 'your cookie secret goes here',
   gmail: {
-    user: 'jeff5120721@gmail.com',
-    password: 'aadtkxjwwgxujroh'
+    user: 'ntueeplus2020@gmail.com',
+    password: 'hkflkyylafkimboz'
   }
 };
