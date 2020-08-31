06e1d58935fa4dd85d52e76a55b630d0520b01df
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-08-31 13:47:14 +0800

    add Policy to /Policy (for FB login)

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Login from './out/Login';
 import './App.css'
 import {NavBar} from './component/AppBar';
 import Footer from './component/Footer/Footer';
-
+import Policy_page from './out/Policy';
 // import { get } from 'mongoose';
 // import equal from 'fast-deep-equal';
 //import {fakeAuth} from './auth';
@@ -125,6 +125,7 @@ const App = () =>{
         {/* <PrivateRoute path="/in" component={Main_in_page} loginstatus={this.state.isLogin}/> */}
         <Route path="/Team" component={Team_page}/>
         <Route path="/History" component={History_page}/>
+        <Route path="/Policy" component={Policy_page}/>
         <Footer/>
       </div>
     );
