1054cdacc583777ae681466d0c205e33ae1c21b0
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-08-31 13:25:51 +0800

    transform-runtime added

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     "presets": ["@babel/preset-env", "@babel/preset-react"],
-    "plugins": ["@babel/plugin-proposal-class-properties"]
+    "plugins": ["@babel/plugin-proposal-class-properties",["transform-runtime", {
+        "helpers": false,
+        "polyfill": false,
+        "regenerator": true,
+        "moduleName": "babel-runtime"
+      }]]
 }
