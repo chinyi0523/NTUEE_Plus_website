76d28505fd807e49b048e9bdb6e58e4f33eaaa09
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2020-11-17 09:26:20 +0800

    isAuth with NODE_ENV

--- routes/srcs/in/isAuth.js
+++ routes/srcs/in/isAuth.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
     const session_account = req.session.loginAccount;
     if(session_account){
-        next();
+        next()
     }else{
-        //return res.status(403).send({description:"請登入"}); 
         console.log("未登入");
-        next();
+        if(process.env.NODE_ENV==='development') next()
+        else return res.status(403).send({description:"請登入"}) 
     }
 }
