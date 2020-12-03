bf63a5e15d6d8c0fa501a322bc14a193a82fc1e7
Yee-Siang <ccs1419@gmail.com>
2020-12-03 16:52:55 +0800

    commit change

--- routes/study/user_studies.js
+++ routes/study/user_studies.js
@@ -3,9 +3,28 @@ const mongoose = require('./study_db'),
 
 const Study_Schema = new Schema({
     ID : String,
-	title : String,
-	subtitle: String,
-	description: String
+	time : String,
+	name: String,
+    gender: String,
+    studentId: String,
+    email: String,
+    applicationTime: String,
+    degree: String,
+    grade: String,
+    ranking: String,
+    professor: String,
+    paper: String,
+    paperTopic: String,
+    firstSchool: String,
+    secondSchool: String,
+    thirdSchool: String,
+    research: String,
+    scope: String,
+    admissionTime: String,
+    professorHoped: String, 
+    Facebook: String,
+    Linkedln: String,
+    else: String
 })
 
 module.exports = mongoose.model('User_Study',Study_Schema);
