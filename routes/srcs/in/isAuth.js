42ffbd290303d5e1da4886095baed8c099081b23
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-02-03 16:44:08 +0800

    env

--- routes/srcs/in/isAuth.js
+++ routes/srcs/in/isAuth.js
@@ -1,12 +1,14 @@
 const { ErrorHandler } = require("../../error");
+const env = require('dotenv')
+env.config()
 
 module.exports = (req, res, next) => {
     const session_account = req.session.loginAccount;
     if(session_account){
         next()
     }else{
-        console.log("未登入")
-        if(process.env.NODE_ENV==='development' || true) next()
+        console.log("not login.",process.env.NODE_ENV==='development'&&'dev mode,accept')
+        if(process.env.NODE_ENV==='development') next()
         else throw new ErrorHandler(403,'未登入')
     }
 }
