35ac967f8f76db0a2a8a4cf8119cafeb00e24755
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-02-05 20:20:25 +0800

    doc

--- routes/srcs/in/study/runMatch/main.js
+++ routes/srcs/in/study/runMatch/main.js
@@ -2,6 +2,19 @@ const asyncHandle = require('express-async-handler')
 const parseExcel = require('./parseExcel')
 const matching = require('./matching')
 
+/**
+ * @api {post} /study_matching 配對
+ * @apiName Study_matching
+ * @apiGroup In/study
+ * @apiDescription 給學長姊跟學弟妹留學配對的.xlsx檔，幫他們配對
+ * 
+ * @apiHeaderExample {json} config
+                 { "content-type": "multipart/form-data" }
+ * @apiParam {File} senior 學長姐的.xlsx
+ * @apiParam {File} junior 學弟妹的.xlsx
+ * 
+ * @apiSuccess (200) {File} - output.xlsx
+ */
 const post = asyncHandle(async (req,res,next)=>{
     const senior = req.files['senior'][0]
     const junior = req.files['junior'][0]
