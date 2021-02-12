a5518dd2a30aa5eb3c8fe3f45bd797806ffa209f
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-02-12 20:30:51 +0800

    cors

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ mongoose.connection.on('open',()=>{
 	//post, get時的解碼
 	app.use(bodyParser.urlencoded({ extended: true }))
 	app.use(bodyParser.json())
-	app.use(cors({
-		origin: 'http://localhost:3000',
-		methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH', 'DELETE'],
-		credentials: true
-	}))
+	app.use(function(req, res, next) {
+		res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+		res.header("Access-Control-Allow-Credentials", true);
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
+		next();
+	});
+	// app.use(cors({
+	// 	origin: '*',//'http://localhost:3000',
+	// 	methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'PATCH', 'DELETE'],
+	// 	credentials: true
+	// }))
 	//session 設定
 	//參考網站https://www.cnblogs.com/chyingp/p/nodejs-learning-express-session.html
 	app.use(
