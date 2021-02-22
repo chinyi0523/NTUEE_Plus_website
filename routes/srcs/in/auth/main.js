d8c26fe3b5df51461c15b892b01c1be0ffb6fb5f
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-02-22 18:12:58 +0800

    reg check done

--- routes/srcs/in/auth/main.js
+++ routes/srcs/in/auth/main.js
@@ -2,5 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post('/manageAuth',require('./manageAuth'))
+router.post('/handlePending',require('./handlePending'))
+router.post('/showPending',require('./showPending'))
 
 module.exports = router
