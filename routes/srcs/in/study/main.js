62c7d92387a08541f14319c52e1a8a161161e524
Claude0311 <ck1041016@gl.ck.tp.edu.tw>
2021-02-23 14:26:42 +0800

    router_auth

--- routes/srcs/in/study/main.js
+++ routes/srcs/in/study/main.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const router_auth = express.Router()
 
-router.post('/study_matching',require('./runMatch/main'))
+router_auth.post('/addLink',require('./addLink'))
+router_auth.post('/study_matching',require('./runMatch/main'))
 
-module.exports = router
+module.exports = {router,router_auth}
